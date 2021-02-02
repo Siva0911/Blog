ba277cb91e286b2ef733843c437018d6b6f425a3
Sivaprasad Srikanthan <Sivaprasad@users.noreply.github.com>
2021-02-02 21:18:59 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
+
+
+const Home = () => {  
+  const {data,isPending,error}=useFetch('http://localhost:8000/blogs')
+
+  
+
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { data && <Bloglist blogs={data} title="All Blogs!"/> }
+    </div>
+  );
+}
+ 
+export default Home;
